6cf0929dd2468446d4713c43d80ded893773476a
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2021-06-16 11:03:54 -0300

    CONFIGURACIONES AVANZADAS CON TAILWIND

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,29 @@
 // postcss.config.js
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
+/* module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ]
+} */
+
+const postcssConfig = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 }
+
+if(process.env.NODE_ENV === 'production'){
+  postcssConfig.plugins.push(
+    require('cssnano')({
+      preset: 'default',
+    })
+  );
+}
+
+module.exports = postcssConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,41 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  purge: [
+    './**/*.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       width: {
         '520': '520px',
-      }
-    },
+      }, 
+      spacing:{
+        '420': '420px'
+      }, 
+      colors:{
+        orange: colors.orange
+      },
+      fontFamily: {
+        sans: [
+          'Inconsolata',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ]
+      },
+    }
   },
   variants: {
     extend: {
